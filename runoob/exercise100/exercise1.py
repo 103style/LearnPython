6b08c1150c35335863686020af8751c6f7e37d3b
103style <103style@github.com>
2018-08-16 19:26:44 +0800

    菜鸟教程 python练习100例  第一题

---
+++ runoob/exercise100/exercise1.py
@@ -0,0 +1,21 @@
+# 题目：有四个数字：1、2、3、4，能组成多少个互不相同且无重复数字的三位数？各是多少？
+L = []
+for x in range(1, 5):
+    for y in range(1, 5):
+        for z in range(1, 5):
+            if (x == y or y == z or x == z):
+                continue
+            else:
+                L.append(100 * x + 10 * y + z)
+print(len(L))
+print(L)
+
+# -*- coding: UTF-8 -*-
+t = 0
+for i in range(1, 5):
+    for j in range(1, 5):
+        for k in range(1, 5):
+            if (i != k) and (i != j) and (j != k):
+                print(i, j, k)
+                t += 1
+print(t)
