e47b691c087adfa76deab925b5ef908a3b4b0b53
103style <wtimexiaoke@gmail.com>
2018-08-17 11:32:21 +0800

    list排序

--- runoob/exercise100/exercise5.py
+++ runoob/exercise100/exercise5.py
@@ -3,8 +3,19 @@
 # 题目：输入三个整数x,y,z，请把这三个数由小到大输出。
 
 L = []
-for x in range(0,3):
+for x in range(0, 3):
     x = int(input("integer:"))
     L.append(x)
 L.sort()
 print(L)
+
+
+# 网站 2.X 版本源码
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+# l = []
+# for i in range(3):
+#     x = int(raw_input('integer:\n'))
+# l.append(x)
+# l.sort()
+# print l
