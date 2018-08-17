570710cc66b2ed91b7b405865e3ec380794a9a70
luoxiaoke <wtimexiaoke@gmail.com>
2018-08-17 10:57:07 +0800

    四个数字能组成多少个互不相同且无重复数字的三位数

--- runoob/exercise100/exercise1.py
+++ runoob/exercise100/exercise1.py
@@ -1,6 +1,7 @@
 # http://www.runoob.com/python/python-exercise-example1.html
 
-# 题目：有四个数字：1、2、3、4，能组成多少个互不相同且无重复数字的三位数？各是多少？
+# 题目：有四个数字：1、2、3、4，
+# 能组成多少个互不相同且无重复数字的三位数？各是多少？
 L = []
 for x in range(1, 5):
     for y in range(1, 5):
