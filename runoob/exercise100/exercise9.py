4ebe3369e6f298319b39c79801eda4596e52cc6c
103style <wtimexiaoke@gmail.com>
2018-08-18 11:06:45 +0800

    sleep 和 获取当前时间

---
+++ runoob/exercise100/exercise9.py
@@ -0,0 +1,15 @@
+# http://www.runoob.com/python/python-exercise-example9.html
+
+# 题目：暂停一秒输出。
+
+import datetime
+import time
+
+print(datetime.datetime.now())
+time.sleep(1)
+print(datetime.datetime.now())
+
+
+# datetime.datetime.now() 获取当前时间
+
+# time.sleep(x) 休眠x秒
