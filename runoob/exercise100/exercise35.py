14725068509a3907300b1d11b064286928f5ad0c
103style <wtimexiaoke@gmail.com>
2018-08-21 10:37:19 +0800

    设置文本颜色 '\033[94m' = BLUE

---
+++ runoob/exercise100/exercise35.py
@@ -0,0 +1,33 @@
+# http://www.runoob.com/python/python-exercise-example35.html
+
+# 题目：文本颜色设置。
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+print(bcolors.WARNING + "警告的颜色字体?" + bcolors.ENDC)
+
+
+class textColor:
+    HEADER = '\033[95m'
+    BlUE = '\033[94m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+print(textColor.UNDERLINE + "下划线测试" + textColor.ENDC)
+print(textColor.BlUE + "BLUE" + textColor.ENDC)
+print(textColor.GREEN + "GREEN" + textColor.ENDC)
+print(textColor.FAIL + "FAIL" + textColor.ENDC)
+print(textColor.BOLD + "BOLD" + textColor.ENDC)
