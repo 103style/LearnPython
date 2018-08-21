e4388f8e9f2501e72265244492ff8bf8767539c5
103style <wtimexiaoke@gmail.com>
2018-08-21 12:09:23 +0800

    auto 定义变量

---
+++ runoob/exercise100/exercise42.py
@@ -0,0 +1,17 @@
+# http://www.runoob.com/python/python-exercise-example42.html
+
+# 题目：学习使用auto定义变量的用法。
+
+num = 2
+
+
+def autofunc():
+    num = 1
+    print('internal block num = %d' % num)
+    num += 1
+
+
+for i in range(3):
+    print('The num = %d' % num)
+    num += 1
+    autofunc()
