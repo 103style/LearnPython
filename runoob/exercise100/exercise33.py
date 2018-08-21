beaefcfa470d1f465f3f5a204856ec481ec0263e
103style <wtimexiaoke@gmail.com>
2018-08-21 10:29:44 +0800

    str.join([]) 在列表中间插入字符

---
+++ runoob/exercise100/exercise33.py
@@ -0,0 +1,11 @@
+# http://www.runoob.com/python/python-exercise-example33.html
+
+# 题目：按逗号分隔列表。
+
+L = [1,2,3,4,5]
+s1 = ','.join(str(n) for n in L)
+print(s1)
+
+help(''.join)
+
+print('-'.join(['2018','08','21']))
