f16d8d115605742d91980fb668171131c6a38b37
103style <wtimexiaoke@gmail.com>
2018-08-22 11:16:56 +0800

    使用tkinter 画圆

--- runoob/exercise100/exercise56.py
+++ runoob/exercise100/exercise56.py
@@ -5,8 +5,8 @@
 
 from tkinter import *
 
-canvas = Canvas(width=800,height=600,bg='red')
-canvas.pack(expand=YES,fill=BOTH)
+canvas = Canvas(width=800, height=600, bg='red')
+canvas.pack(expand=YES, fill=BOTH)
 k = 1
 j = 1
 for i in range(0, 16):
