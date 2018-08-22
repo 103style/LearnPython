fdd89c80475969e449ed70619189333928721562
103style <wtimexiaoke@gmail.com>
2018-08-22 11:09:59 +0800

    使用thinter 画圆

---
+++ runoob/exercise100/exercise56.py
@@ -0,0 +1,19 @@
+# http://www.runoob.com/python/python-exercise-example56.html
+
+# 题目：画图，学用circle画圆形。　　　
+
+
+from tkinter import *
+
+canvas = Canvas(width=800,height=600,bg='red')
+canvas.pack(expand=YES,fill=BOTH)
+k = 1
+j = 1
+for i in range(0, 16):
+    canvas.create_oval(310 - k, 250 - k, 310 + k, 250 + k, width=1)
+    k += j
+    j += 0.8
+
+help(canvas.create_oval)
+
+mainloop()
