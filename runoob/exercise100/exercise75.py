dd8698d562e5d5561be23c6c6b6052b9766e8021
103style <wtimexiaoke@gmail.com>
2018-08-24 11:07:18 +0800

    题目是啥？ 复制了答案过来

---
+++ runoob/exercise100/exercise75.py
@@ -0,0 +1,18 @@
+# http://www.runoob.com/python/python-exercise-example75.html
+
+
+# 题目：放松一下，算一道简单的题目。
+
+
+for i in range(5):
+    n = 0
+    if i != 1:
+        n += 1
+    if i == 3:
+        n += 1
+    if i == 4:
+        n += 1
+    if i != 4:
+        n += 1
+    if n == 3:
+        print(64 + i)
