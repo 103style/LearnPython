4bbb58cd2b5403ced59d43f9b91f604eee2f2d71
103style <wtimexiaoke@gmail.com>
2018-08-24 11:23:16 +0800

    字符串排序

---
+++ runoob/exercise100/exercise79.py
@@ -0,0 +1,28 @@
+# http://www.runoob.com/python/python-exercise-example79.html
+
+
+# 题目：字符串排序。
+
+a  = 'a'
+b = 'b'
+if a >= b:
+    print(a)
+else:
+    print(b)
+
+a  = 'ca'
+b = 'cb'
+if a >= b:
+    print(a)
+else:
+    print(b)
+
+a  = 'ca'
+b = 'ca1'
+if a >= b:
+    print(a)
+else:
+    print(b)
+
+
+# 字符串排序根绝字母的ASCII码大小顺序来排的
