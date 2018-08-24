ff89af2e3dca06c23dedfdc695dc14f258c2dbb6
103style <wtimexiaoke@gmail.com>
2018-08-24 17:13:05 +0800

    传给函数的变量，在函数里面改变了 再在主函数条用会是函数改变之后的值

---
+++ runoob/exercise100/exercise87.py
@@ -0,0 +1,19 @@
+# http://www.runoob.com/python/python-exercise-example87.html
+
+# 题目：回答结果（结构体变量传递）。
+
+class student:
+    x = 0
+    c = 0
+
+
+def f(stu):
+    stu.x = 20
+    stu.c = 'c'
+
+
+a = student()
+a.x = 3
+a.c = 'a'
+f(a)
+print(a.x, a.c)
