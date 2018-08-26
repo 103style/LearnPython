2d1d747f95f6d6127c5ff60b44fe4286e013e21d
103style <wtimexiaoke@gmail.com>
2018-08-26 22:52:02 +0800

    使用str.count(str1)计算str中出现str1的次数

---
+++ runoob/exercise100/exercise96.py
@@ -0,0 +1,9 @@
+# http://www.runoob.com/python/python-exercise-example96.html
+
+# 题目：计算字符串中子串出现的次数。
+
+
+str1 = input('请输入一个字符串:\n')
+str2 = input('请输入一个子字符串:\n')
+count = str1.count(str2)
+print(count)
